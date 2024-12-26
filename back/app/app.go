92fa935855150e94d66414cd12c68379153c03bd
Daniel Osorio Cárdenas <d.osorioc2@uniandes.edu.co>
2024-12-25 20:03:11 -0500

    Graceful shutdown implemented

--- back/app/app.go
+++ back/app/app.go
@@ -4,15 +4,18 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"time"
 )
 
 type App struct {
-	router http.Handler
+	router   http.Handler
+	dbClient *http.Client
 }
 
 func New() *App {
 	return &App{
-		router: loadRoutes(),
+		router:   loadRoutes(),
+		dbClient: &http.Client{},
 	}
 }
 
@@ -21,9 +24,31 @@ func (a *App) Start(ctx context.Context) error {
 		Addr:    ":3000",
 		Handler: a.router,
 	}
-	err := server.ListenAndServe()
+	// Ping the database to check if it's up
+	_, err := a.dbClient.Get("http://localhost:4080")
 	if err != nil {
-		return fmt.Errorf("failed to start server: %w", err)
+		return fmt.Errorf("failed to ping database: %w", err)
 	}
-	return nil
+
+	fmt.Println("Database is up. Starting server...")
+
+	ch := make(chan error, 1)
+
+	go func() {
+		err = server.ListenAndServe()
+		if err != nil {
+			ch <- fmt.Errorf("failed to start server: %w", err)
+		}
+		close(ch)
+	}()
+
+	select {
+	case err = <-ch:
+		return err
+	case <-ctx.Done():
+		ctxTimeout, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		return server.Shutdown(ctxTimeout)
+	}
+
 }
--- back/main.go
+++ back/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
 
 	"github.com/DanielOsorio01/enron-email-search/back/app"
 )
@@ -10,8 +12,14 @@ import (
 func main() {
 	app := app.New()
 
-	err := app.Start(context.TODO())
+	// Define a context that will be canceled when a SIGINT is sent
+	// to have graceful shutdown
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	// Start the app
+	err := app.Start(ctx)
 	if err != nil {
-		fmt.Println("failed to start app: %w", err)
+		fmt.Println("failed to start app:", err)
 	}
 }
